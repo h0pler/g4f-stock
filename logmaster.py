1d1f7191145ecfe186f2a915ba75c0143d6fbe73
h0pler <me@h0pler.xyz>
2024-01-24 09:03:29 +0000

    logmaster.py

---
+++ logmaster.py
@@ -0,0 +1,14 @@
+import aiofiles
+import datetime
+
+logfile = datetime.datetime.now().strftime("logs/%y%m%d-%H:%M:%S.log")
+
+
+async def log_start():
+    async with aiofiles.open(logfile, "a") as file:
+        await file.write(f"\n\n================= [LOG] =================\n")
+
+
+async def log_print(prefix, message):
+    async with aiofiles.open(logfile, "a") as file:
+        await file.write(prefix + "  " + message + "\n")
