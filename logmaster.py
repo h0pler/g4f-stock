4014109348f5d7fe676ac03dd1ec340c922a238f
h0pler <h0pler@proton.me>
2024-01-25 02:54:19 +0900

    no aiofiles

--- logmaster.py
+++ logmaster.py
@@ -1,19 +1,18 @@
-import aiofiles
 import datetime
 
 logfile = datetime.datetime.now().strftime("logs/%y%m%d-%H:%M:%S.log")
 
 
-async def log_start():
-    async with aiofiles.open(logfile, "a") as file:
-        await file.write(f"\n\n================= [LOG] =================\n")
+def log_start():
+    with open(logfile, "a") as file:
+        file.write("\n\n================= [LOG] =================\n")
 
 
-async def log_print(prefix, message):
-    async with aiofiles.open(logfile, "a") as file:
-        await file.write(prefix + "  " + message + "\n")
-        
+def log_print(prefix, message):
+    with open(logfile, "a") as file:
+        file.write(prefix + "  " + message + "\n")
 
-async def log_end():
-    async with aiofiles.open(logfile, "a") as file:
-        await file.write(f"\n================= [END] =================\n")
+
+def log_end():
+    with open(logfile, "a") as file:
+        file.write("\n================= [END] =================\n")
