1ac96a6051dfe11167a451aeebf98f79ef6f2e07
h0pler <h0pler@proton.me>
2024-01-25 02:58:44 +0900

    make logfile directory

--- logmaster.py
+++ logmaster.py
@@ -1,18 +1,23 @@
+import os
 import datetime
 
 logfile = datetime.datetime.now().strftime("logs/%y%m%d-%H:%M:%S.log")
 
+def create_logs_directory():
+    if not os.path.exists("logs"):
+        os.makedirs("logs")
 
 def log_start():
+    create_logs_directory()
     with open(logfile, "a") as file:
         file.write("\n\n================= [LOG] =================\n")
 
-
 def log_print(prefix, message):
+    create_logs_directory()
     with open(logfile, "a") as file:
         file.write(prefix + "  " + message + "\n")
 
-
 def log_end():
+    create_logs_directory()
     with open(logfile, "a") as file:
         file.write("\n================= [END] =================\n")
